50b680b7c3659297ac62c41f993e7a99d7a07aa5
Dmitry Sokolov <dsokolov@smass.tech>
2019-03-01 13:00:12 +0400

    assignment 1

--- assignments/assignment1/knn.py
+++ assignments/assignment1/knn.py
@@ -54,8 +54,8 @@ class KNN:
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
             for i_train in range(num_train):
-                # TODO: Fill dists[i_test][i_train]
-                pass
+                dists[i_test][i_train] = np.sum(np.abs(X[i_test] - self.train_X[i_train]))
+        return dists
 
     def compute_distances_one_loop(self, X):
         '''
@@ -73,9 +73,8 @@ class KNN:
         num_test = X.shape[0]
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
-            # TODO: Fill the whole row of dists[i_test]
-            # without additional loops
-            pass
+            dists[i_test] = np.sum(np.abs(X[i_test] - self.train_X), axis=1)
+        return dists
 
     def compute_distances_no_loops(self, X):
         '''
@@ -93,8 +92,10 @@ class KNN:
         num_test = X.shape[0]
         # Using float32 to to save memory - the default is float64
         dists = np.zeros((num_test, num_train), np.float32)
-        # TODO: Implement computing all distances with no loops!
-        pass
+        # TODO: а как сделать-то без цикла???
+        for i_test in range(num_test):
+            dists[i_test] = np.sum(np.abs(X[i_test] - self.train_X), axis=1)
+        return dists
 
     def predict_labels_binary(self, dists):
         '''
@@ -111,9 +112,7 @@ class KNN:
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.bool)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
-            # nearest training samples
-            pass
+            pred[i] = True
         return pred
 
     def predict_labels_multiclass(self, dists):
@@ -132,7 +131,5 @@ class KNN:
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.int)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
-            # nearest training samples
-            pass
+            pred[i] = True
         return pred
